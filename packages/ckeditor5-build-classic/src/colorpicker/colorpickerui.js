6b8b1546e3bcdde255f19e34967a4b8bd0621f6c
Rastafan <rastafanimplacheibol@libero.it>
2022-10-05 16:25:23 +0200

    merge edits that were in stable

--- packages/ckeditor5-build-classic/src/colorpicker/colorpickerui.js
+++ packages/ckeditor5-build-classic/src/colorpicker/colorpickerui.js
@@ -11,6 +11,7 @@ import DropdownButtonView from '@ckeditor/ckeditor5-ui/src/dropdown/button/dropd
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import { FONT_COLOR, renderDowncastElement, renderUpcastAttribute } from '@ckeditor/ckeditor5-font/src/utils';
+import clickOutsideHandler from '@ckeditor/ckeditor5-ui/src/bindings/clickoutsidehandler';
 
 import fontColorIcon from '@ckeditor/ckeditor5-font/theme/icons/font-color.svg';
 
@@ -99,14 +100,42 @@ export default class ColorPickerUI extends Plugin {
 				icon: fontColorIcon
 			});
 
-			dropdownView.render();
-
 			const inputview = new ColorpickerInputView(locale, {
 				columns: 5,
 				removeButtonLabel: t('Remove color'),
 				documentColorsLabel: t('Document colors'),
 				documentColorsCount: 10
 			});
+
+
+			dropdownView.on('render', () => {
+
+				var contextElements = [dropdownView.element];
+				var pickers = document.getElementsByClassName('jscolor-picker');
+
+				if (pickers && pickers[0]) {
+					console.log('AGGIUNGO L\'ELEMENTO : ', pickers[0]);
+					contextElements.push(pickers[0]);
+				}
+
+
+				clickOutsideHandler({
+					emitter: dropdownView,
+					activator: () => dropdownView.isOpen,
+					callback: () => {
+						// console.log('editor.ui.focusTracker.isFocused : ', editor.ui.focusTracker.isFocused);
+						// console.log('editor : ', editor);
+						// console.log('dropdownView : ', dropdownView);
+						// console.log('inputview : ', inputview);
+						// if (editor.ui.focusTracker.isFocused) {
+						dropdownView.isOpen = false;
+						// }
+					},
+					contextElements: contextElements
+				});
+			});
+
+			dropdownView.render();
 
 			dropdownView.panelView.children.add(inputview);
 
@@ -115,12 +144,30 @@ export default class ColorPickerUI extends Plugin {
 			inputview.on('input', (eventInfo, event) => {
 				dropdownView.fire('execute', event);
 			});
-			
+
 			dropdownView.bind('isEnabled').to(editor.commands.get(FONT_COLOR));
 
 			dropdownView.on('execute', (evt, data) => {
 				editor.execute(FONT_COLOR, data);
 				//editor.editing.view.focus();
+			});
+
+			dropdownView.on('change:isOpen', (evt, name, isVisible) => {
+				if (isVisible) {
+
+					// Se non già costruito, si costruisce il picker del colore.
+					// Lo facciamo quì per poter renderizzare il box dei colori nello stesso contenitore dell'input,
+					// ed impedirgli di perdere il focus quando si cambia colore.
+					if (!inputview.staticColorsGrid) {
+						var inputEls = inputview.element.getElementsByTagName('input');
+						if (inputEls && inputEls[0]) {
+							inputview.staticColorsGrid = new JSColor(inputEls[0], { container: inputview.element });
+						} else {
+							console.warn('INPUT COLOR NON TROVATO');
+						}
+					}
+					inputview.updateSelectedColors();
+				}
 			});
 
 			return dropdownView;
